51b4e8d7e0dad14fc9ef786cb3b4d8846b4fb372
SlavikChyzh <110846906+SlavikChyzh@users.noreply.github.com>
2023-01-16 19:51:29 +0200

    3
    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,11 +2,36 @@ import { save, load } from './localStorage';
 import throttle from 'lodash.throttle';
 
 const localStorageKey = 'feedback-form-state';
+let storeData = {
+  messageStore: '',
+  mailStore: '',
+};
 const form = document.getElementsByClassName('feedback-form');
-const email = document.getElementsByName('email');
-const message = document.getElementsByName('message');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
 const submitBtn = document.querySelector('button');
-console.log(form);
-console.log(email);
-console.log(message);
-console.log(submitBtn);
+
+message.textContent = load(localStorageKey).messageStore;
+email.value = load(localStorageKey).mailStore;
+
+message.addEventListener('input', event => {
+  //console.log(email.textContent);
+  console.log(event.currentTarget.value);
+  storeData.messageStore = event.currentTarget.value;
+  save(localStorageKey, storeData);
+});
+email.addEventListener('input', event => {
+  //console.log(email.textContent);
+  console.log(event.currentTarget.value);
+  storeData.mailStore = event.currentTarget.value;
+  save(localStorageKey, storeData);
+});
+
+// message.addEventListener('input', event => {
+//   save(localStorageKey, {
+//     emailStorage: email.values,
+//     messageStorage: message.values,
+//   });
+//   console.log(email.values);
+//   console.log(message.values);
+// });
